c86909fdd332325b5d9650cd0e7d8d0c819fd7f1
Sandeep <sandipsharma2445@gmail.com>
2025-06-16 10:48:04 +0530

    header component created

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,17 +1,12 @@
 import { Outlet } from "react-router-dom";
 
-import { Button } from "../ui/button";
-import Logo from "../ui/Logo";
+import Header from "./Header";
 
 const Layout = () => {
     return (
-        <div className="flex flex-col justify-center items-center min-h-screen">
-            <Logo />
+        <div className="flex flex-col min-h-screen w-full">
+            <Header />
             <Outlet />
-            <div className="flex gap-2 mt-3">
-                <Button>Get Started</Button>
-                <Button variant="secondary">View Website</Button>
-            </div>
         </div>
     )
 }
