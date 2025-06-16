dcd8274ae0e9e2d8adbcac4ccc12e2b7e7ce93a5
Sandeep <sandipsharma2445@gmail.com>
2025-06-16 10:37:46 +0530

    logo created

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 
 import { Button } from "../ui/button";
+import Logo from "../ui/Logo";
 
 const Layout = () => {
     return (
         <div className="flex flex-col justify-center items-center min-h-screen">
-            <h1 className="text-3xl font-bold">Credify</h1>
+            <Logo />
             <Outlet />
             <div className="flex gap-2 mt-3">
                 <Button>Get Started</Button>
